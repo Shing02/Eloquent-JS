24f76f95f2daf8fdf70168bc7b6091e6e9ba8e1a
Shing02 <samu.mollet@gmail.com>
2019-03-06 01:05:25 +0100

    Create Recursion.js

---
+++ Functions/Recursion.js
@@ -0,0 +1,17 @@
+function isEven(number) {
+
+  if (number < 0) {
+  return isEven(-number);
+  }
+  
+  if (number === 0) {
+  return true;
+  }
+  
+  if (number === 1) {
+  return false;
+  }
+  
+  return isEven(number - 2);
+  
+}
