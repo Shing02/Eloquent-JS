6d7dbc052bfd48ef34e38d12013c34d22c7d612b
Shing02 <samu.mollet@gmail.com>
2019-03-06 23:03:53 +0100

    Update range.js

--- Data Structures Objects and Arrays/The sum of the range/range.js
+++ Data Structures Objects and Arrays/The sum of the range/range.js
@@ -1,10 +1,20 @@
 function range(start, end) {
-
+  
   let arr = [];
-  for (let i = start; i <= end; i++) {
+  
+  if (start < end) {
+  	for (let i = start; i <= end; i++) {
     arr.push(i);
+  	} 
   }
   
-return arr;
-
+  if (start > end) {
+  	for (let i = start; i >= end; i--) {
+    arr.push(i);
+  	}
+  }
+  
+  return arr;
+  
 }
+  
