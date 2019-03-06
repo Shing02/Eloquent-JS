33607722b785dbb47294e4ce14a2faa2b47d2378
Shing02 <samu.mollet@gmail.com>
2019-03-06 23:53:35 +0100

    Update rangeModified.js

--- Data Structures Objects and Arrays/The sum of the range/rangeModified.js
+++ Data Structures Objects and Arrays/The sum of the range/rangeModified.js
@@ -3,16 +3,16 @@ function range(start, end, step) {
 let arr = [];
 
 function oldRange(start, end) {  
-	if (start < end) {
-  	for (let i = start; i <= end; i++) {
-    	arr.push(i);
-  	} 
+  if (start < end) {
+    for (let i = start; i <= end; i++) {
+      arr.push(i);
+    } 
   }
   
-if (start > end) {
-  for (let i = start; i >= end; i--) {
-    arr.push(i);
-  	}
+  if (start > end) {
+    for (let i = start; i >= end; i--) {
+      arr.push(i);
+    }
   }
 }
       
@@ -21,14 +21,14 @@ if (step === undefined) {
 }
 
 if (step < 0) {
-	for (let i = start; i >= end; i += step) {
+  for (let i = start; i >= end; i += step) {
     arr.push(i);
   }
 }
 
 if (step > 0) {
- 	for (let i = start; i <= end; i += step) {
-  	arr.push(i);
+  for (let i = start; i <= end; i += step) {
+    arr.push(i);
   }
 }
     
