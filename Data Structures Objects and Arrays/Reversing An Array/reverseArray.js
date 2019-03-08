ff5855704e7c7a5574d5b43e1afacde9cf20074f
Shing02 <samu.mollet@gmail.com>
2019-03-08 03:00:56 +0100

    Update reverseArray.js

--- Data Structures Objects and Arrays/Reversing An Array/reverseArray.js
+++ Data Structures Objects and Arrays/Reversing An Array/reverseArray.js
@@ -1,3 +1,4 @@
+// Code is self explanatory on this one.
 
 function reverseArray(arg) { 
 
@@ -12,7 +13,9 @@ function reverseArray(arg) {
   
 }
 
-
+/* the reverseArrayInPlace function takes the first (array[n-(n+1)]) and the last (array[n-1]) value of a given array and swaps 
+them. Then it proceeds to replicate this mechanic with array[n-(n+2)] and array[n-2], etc. It doesn't matter if n is even or odd,
+the number of permutations will be Math.floor(arg.length) / 2. */
 
 function reverseArrayInPlace(arg){
 
