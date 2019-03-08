11b45eb3b2db71a9ac113aa4b04407c335de314f
Shing02 <samu.mollet@gmail.com>
2019-03-08 03:10:40 +0100

    Update countChar.js

--- Functions/countChar.js
+++ Functions/countChar.js
@@ -1,3 +1,7 @@
+/* The indexOf(param,index) allows us to look for a character from a certain index in a string. It returns the number of the index 
+if found, and it returns -1 if not. The while loop forces the indexOf built in function to increment its index each time a
+character is found and to search again. */
+
 function countBS(str, param) {
 
   let count = 0;
