09740af0437095cea87f9dfb44df857aa363dc40
Shing02 <samu.mollet@gmail.com>
2019-03-12 01:34:21 +0100

    Update nth.js

--- Data Structures Objects and Arrays/A list/nth.js
+++ Data Structures Objects and Arrays/A list/nth.js
@@ -10,19 +10,6 @@ function listToArray(list) {
   
 }
 
-function arrayToList(array) {
-  
-  let list = null;
-  for (let i = array.length-1; i >= 0; i--) {
-    list = { 
-      value : array[i],
-      rest : list
-    }
-  }
-  
-  return list;
-    
-}
 
 //Academic implementation
 function nth(list, n) {
