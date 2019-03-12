ec290f0188f6308cc880655610bb13e3add8c4df
Shing02 <samu.mollet@gmail.com>
2019-03-12 01:33:55 +0100

    Update nth.js

--- Data Structures Objects and Arrays/A list/nth.js
+++ Data Structures Objects and Arrays/A list/nth.js
@@ -24,6 +24,17 @@ function arrayToList(array) {
     
 }
 
+//Academic implementation
+function nth(list, n) {
+  if (!list) // case if n > list number
+    return undefined;
+  else if (n == 0)
+    return list.value;
+  else
+    return nth(list.rest, n - 1);
+}
+
+//My original attempt with a loop
 function nth(list, number) {
   const array = listToArray(list);
   for (let i = 0; i < array.length-1; i++) {
