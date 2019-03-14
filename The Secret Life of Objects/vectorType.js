6c866cdcc3481f59d54eb1d6c5f76390284a1930
Shing02 <samu.mollet@gmail.com>
2019-03-14 22:14:25 +0100

    Create vectorType.js

---
+++ The Secret Life of Objects/vectorType.js
@@ -0,0 +1,25 @@
+class Vec {
+
+  constructor(x,y) {
+    this.x = x;
+    this.y = y;
+  }
+  
+  plus(anotherVec) {
+    return {
+      x : this.x + anotherVec.x,
+      y : this.y + anotherVec.y
+    }
+  }
+  
+  minus(anotherVec) {
+    return {
+      x : this.x - anotherVec.x,
+      y : this.y - anotherVec.y
+    }
+  }
+  
+  get length() {
+  	return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+  }
+}
