40bdc2c9609b44c3a059f56a3cef8207dc7a6eab
Shing02 <samu.mollet@gmail.com>
2019-03-14 20:36:36 +0100

    Update chapterExamples.js

--- Higher Order  Functions/chapterExamples.js
+++ Higher Order  Functions/chapterExamples.js
@@ -2,7 +2,7 @@ const SCRIPTS = [
   {
     name: "Coptic",
     ranges: [[994, 1008], [11392, 11508], [11513, 11520]],
-    86direction: "ltr",
+    direction: "ltr",
     year: -200,
     living: false,
     link: "https://en.wikipedia.org/wiki/Coptic_alphabet"
