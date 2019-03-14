9ba740d337c489fb1e61f2889a7b640c98588fce
Shing02 <samu.mollet@gmail.com>
2019-03-14 22:17:36 +0100

    Update vectorType.js

--- The Secret Life of Objects/vectorType.js
+++ The Secret Life of Objects/vectorType.js
@@ -6,20 +6,14 @@ class Vec {
   }
   
   plus(anotherVec) {
-    return {
-      x : this.x + anotherVec.x,
-      y : this.y + anotherVec.y
+    return new Vec( x : this.x + anotherVec.x, y : this.y + anotherVec.y);
     }
-  }
   
   minus(anotherVec) {
-    return {
-      x : this.x - anotherVec.x,
-      y : this.y - anotherVec.y
+    return new Vec( x : this.x - anotherVec.x, y : this.y - anotherVec.y);
     }
-  }
   
   get length() {
-  	return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
-  }
+    return Math.sqrt(Math.pow(this.x, 2) + Math.pow(this.y, 2));
+    }
 }
