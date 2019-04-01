301865111a565bc28a9c7648c630e6a00815f33b
Shing02 <samu.mollet@gmail.com>
2019-04-02 01:09:05 +0200

    Create everything.js

---
+++ Chapter 5 : Higher Order Functions/everything.js
@@ -0,0 +1,15 @@
+
+function every(array, f) {
+  
+  for (let arr of array) {
+    if (!f(arr)) {
+      return false;
+    }
+    
+    return true;
+    
+    }
+}
+
+
+// No idea for the second implementation with some
